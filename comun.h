6f237f49521560690b082447a8c229da184f6866
Alejo Nicolas Nieva <alejonieva86@gmail.com>
2026-05-10 11:50:04 -0300

    Add files via upload

---
+++ comun.h
@@ -0,0 +1,52 @@
+#ifndef COMUN_H_
+#define COMUN_H_
+
+#include <string.h>
+#include <stdlib.h>
+
+///COLA
+#define TAM_COLA 300
+
+#define COLA_LLENA 1
+#define COLA_NO_LLENA 0
+
+#define COLA_VACIA 1
+#define COLA_NO_VACIA 0
+
+///PILA
+#define TAM_PILA 340
+
+#define PILA_LLENA 1
+#define PILA_NO_LLENA 0
+#define PILA_VACIA 1
+#define PILA_NO_VACIA 0
+#define JOYA 0
+
+#define MOSTRAR 0
+#define NO_MOSTRAR 1
+
+///LISTA
+
+
+#define LISTA_VACIA 1
+#define LISTA_NO_VACIA 0
+
+//ERRORES
+
+#define ERROR -888
+#define NO_ENCONTRADO -777
+
+#define LISTA_NO_LLENA 0
+
+
+#define REALIZAR_ACCION 1
+#define NO_REALIZAR_ACCION 0
+
+///COMUNES
+#define TODO_OK 0
+#define SIN_MEM -999
+#define Minimo(x,y) ( (x) <= (y)? (x) : (y))
+
+
+
+#endif // COMUN_H_
