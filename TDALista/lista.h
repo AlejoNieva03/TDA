0c1ddcdd8ef7281aced0b83e1a5659a751ff2c3c
Alejo Nicolas Nieva <alejonieva86@gmail.com>
2026-05-10 17:10:41 -0300

    Update lista.h

--- TDALista/lista.h
+++ TDALista/lista.h
@@ -52,7 +52,8 @@ int sacarPrimeroLista (tLista *p, void* d, unsigned tamInfo);
 int sacarUltimoLista(tLista *p, void* d, unsigned tamInfo);
 void mostrar (tLista* p, imp IMP);
 ///PARA QUE SEA GEMERICA, DEBO PASARLE PUNTEROS A FUNCIONAA
-int InsertarEnOrden(tLista* p,const void*d, unsigned tamInfo, tComp CMP, int esDup, tAccion ACCION);
+int InsertarEnOrdenSinDupSinAccion(tLista* p,const void*d, unsigned tamInfo, tComp CMP);
+int InsertarEnOrdenDupSinAccion(tLista* p,const void*d, unsigned tamInfo, tComp CMP);
 
 int eliminarElementoSinDupNoOrdenados(tLista*p, void* d, unsigned tamInfo, tComp CMP);
 int eliminarElementoPosN (tLista*p, void* d, unsigned tamInfo, int pos);
